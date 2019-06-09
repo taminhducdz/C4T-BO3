eef319ddfbcb35d8303a3eee4d74c2acd5f1a857
unknown <taminhduc2004@gmail.com>
2019-06-09 12:08:28 +0700

    first commit

---
+++ section1.py
@@ -0,0 +1,9 @@
+# print('tinhdientichtamgiac')
+# chieucao = int(input("nhap chieu cao:"))
+# canhday = int(input("nhap canh day:"))
+# dientichtamgiac = (chieucao*canhday)*0.5
+# # print(dientichtamgiac)
+
+bankinh = float(input("nhap ban kinh: "))
+dientichhinhtron= bankinh*3.14
+print('dien tich hinh tron la: ', dientichhinhtron)
